80f363fa9ec5b09defe750aae155bcd32cfb7cb2
Jeremias <jeregiampieri@gmail.com>
2026-02-02 13:39:41 -0300

    comienzo del maquetado

---
+++ src/assets/styles/GlobalStyle.js
@@ -0,0 +1,15 @@
+import {createGlobalStyle, styled} from "styled-components"
+
+export const GlobalStyle = createGlobalStyle`
+
+/* Variables globales para los colores de la aplicación */
+:root{
+    --orange-bg: #2f2618;
+    --orange: #ff9d01;
+    --magenta: #ff005c;
+    --gray-bg: #2b2b2c;
+    --btn-gradient: linear-gradient(83deg, #ffa100, #fb103d);
+    --btn-gradient-secondary: linear-gradient(140deg, #3B3022, #3B2329);
+}
+
+`
