b04944181da4fe211074a36212d0887dbc7b2cde
Jeremias <jeregiampieri@gmail.com>
2026-03-31 12:25:53 -0300

    actualizaciones

--- src/assets/components/Hero/Hero.jsx
+++ src/assets/components/Hero/Hero.jsx
@@ -2,8 +2,9 @@ import { BotonBuscar, BuscarContainer, BusquedaContainer, HeroStyle, ImgContaine
 import pizzaImg from "../../img/pizzasImg.png"
 import { BiSearchAlt } from "react-icons/bi";
 import {Typewriter} from "react-simple-typewriter"
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { seleccionarCategoria } from "../../redux/categorias/categoriasSlice";
 
 
 // Dentro del home, voy a colocar/importar el hero, ya que va a formar parte del mismo
@@ -13,12 +14,23 @@ export const Hero = () => {
     const {categorias} = useSelector((estado) => {
             return estado.categorias
         })
-    console.log("Categorias:", categorias)
+
+    // Acá estoy llamando al hook dispatch para indicar el uso del reducer
+    const dispatch = useDispatch()
+
     const handlerSubmit = (evento) =>{
+        // Esto se usa para evitar el recargo de la página cada vez que le hacemos click al botón BUSCAR
         evento.preventDefault();
-        console.log("valor:", valor)
         // Para limpiar la data que viene del input
-        // const nuevaCategoria = valor.trim().toLowerCase().split(" ").join("")
+        const nuevaCategoria = valor.trim().toLowerCase().split(" ").join("")
+        const categoriaEncontrada = categorias.find((categoria) => categoria.category.toLowerCase() === nuevaCategoria)
+        if (categoriaEncontrada){
+            dispatch(seleccionarCategoria(categoriaEncontrada.category))
+        }else{
+            alert("Categoría inexistente")
+        }
+        // Esto es para volver a vaciar el input luego de hacer click en BUSCAR, y de paso reseteamos el valor del useState
+        setValor("")
     }
 
     return (
@@ -36,7 +48,7 @@ export const Hero = () => {
                         <BiSearchAlt />
                     </LupaContainer>
                     <form>
-                    <InputBuscar type="text" placeholder="Ej: Pizza a la piedra" 
+                    <InputBuscar type="text" placeholder="Ej: Pizzas a la piedra" 
                     // Acá estoy capturando el valor del input y lo coloco dentro del valor del useState
                     onChange={(e) => {setValor(e.target.value)}}
                     value={valor} />
--- src/assets/components/Productos/Productos.jsx
+++ src/assets/components/Productos/Productos.jsx
@@ -8,6 +8,9 @@ export const Productos = () => {
         return estado.productos
     })
 
+    // Me traigo la categoriaSeleccionada en el estado global
+    const {categoriaSeleccionada} = useSelector((estado) => estado.categorias)
+
     return (
         <ProductosWrapped>
             <h2 style={{fontWeight:400}}>Productos</h2>
@@ -15,9 +18,17 @@ export const Productos = () => {
                 {
                     //Object.entries me crea un array por cada relación propiedad:valor
                     Object.entries(productosPRUEBA).map((array) => {
-                        return array[1].map((producto) => {
+                        if (categoriaSeleccionada){
+                            if (array[0] === categoriaSeleccionada){
+                                return array[1].map((producto) => {
+                                    return <Producto key={producto.id} {...producto}></Producto>
+                            })
+                            }
+                        }else{
+                            return array[1].map((producto) => {
                             return <Producto key={producto.id} {...producto}></Producto>
-                        })
+                            })
+                        }
                     })
                 }
               </ProductosContainer>
